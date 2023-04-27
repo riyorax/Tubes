cef84e801a4255d2486f1ffc299124f9aa813bf8
choidaldall <114424355+choidaldall@users.noreply.github.com>
2023-04-27 07:39:10 +0700

    Update F04-Hilangkan Jin.py
    benerin iterasi

--- F04-Hilangkan Jin.py
+++ F04-Hilangkan Jin.py
@@ -15,37 +15,37 @@ def hilangkan_jin():
     else:
         hapus_jin = input("Masukkan username jin : ")
 
-    i = 0
+        i = 0
 
-    while i <= (data.N_user):
-        if data.users[i][0] == hapus_jin:
-            konfirmasi = input("Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? ")
-            if konfirmasi == "Y" or konfirmasi == 'y':
-                # Hapus jin
-                for i in range(1, data.N_user+1):
-                    if data.users[i][0] == hapus_jin:
-                        data.users[i] = ['', '', '']
-                        for j in range(i, data.N_user):
-                            data.users[j] = data.users[j+1]
-                        break 
-                # Hapus candi jin
-                for i in range (data.N_candi):
-                    if data.users[i][1] == hapus_jin:
-                        data.users[i] = ['', '', '','','']
-                        for j in range(i, data.N_user):
-                            data.users[j] = data.users[j+1]
-                        break    
-                print("Jin telah berhasil dihapus dari alam gaib.")
+        while i <= (data.N_user):
+            if data.users[i][0] == hapus_jin:
+                konfirmasi = input("Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? ")
+                if konfirmasi == "Y" or konfirmasi == 'y':
+                    # Hapus jin
+                    for i in range(1, data.N_user+1):
+                        if data.users[i][0] == hapus_jin:
+                            data.users[i] = ['', '', '']
+                            for j in range(i, data.N_user):
+                                data.users[j] = data.users[j+1]
+                            break 
+                    # Hapus candi jin
+                    for i in range (data.N_candi):
+                        if data.users[i][1] == hapus_jin:
+                            data.users[i] = ['', '', '','','']
+                            for j in range(i, data.N_user):
+                                data.users[j] = data.users[j+1]
+                            break    
+                    print("Jin telah berhasil dihapus dari alam gaib.")
+                    break
+                else: # Konfirmasi == "N" or "n"
+                    break
+            elif i == (data.N_user):
+                print("Tidak ada jin dengan username tersebut.")
                 break
-            else: # Konfirmasi == "N" or "n"
-                break
-        elif i == (data.N_user):
-            print("Tidak ada jin dengan username tersebut.")
-            break
-        else:
-            i += 1
+            else:
+                i += 1
 
 hilangkan_jin()
 
 # Buat cek list data (Hapus aja kalo udah ngecek)
-data.cetak(data.users)
+# data.cetak(data.users)

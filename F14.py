8a7c076ce0aa2124d6b10b1c3df6d39aece3c4ce
Wafi07 <117271546+Wafi07@users.noreply.github.com>
2023-05-01 00:19:39 +0700

    Update F14.py

--- F14.py
+++ F14.py
@@ -3,20 +3,29 @@ import tools
 
 def save(arr_user,arr_bahan_bangunan,arr_candi):
     folder = str(input("Masukkan nama folder :"))
+    print("\nsaving...\n")
+
     ## prosedur mengecek data dan membuat folder
-    path = folder
-    print(path)
-    if os.path.exists(path): # Jika folder ada
-        pass
-    else: # folder tidak ditemukan, maka membuat folder
-        os.makedirs(path)
+    path = "save/" + folder
+
+    ## mengecek folder save
+    if os.path.exists("save"):
+        pass # folder save ada
+        if os.path.exists(path):
+            pass   #folder ada
+
+        else: #folder tidak ada
+            print(f"membuat folder {path}... \n")
+            os.mkdir(path) # buat folder di dalam folder save
+
+    else: # folder tidak ditemukan, maka membuat folder save
+        print(f"membuat folder save...\n")
+        os.mkdir("save") #membuat folder save
+        print(f"membuat folder {path}...\n")
+        os.mkdir(path) # membuat folder
 
     # menyimpan data array ke csv 
-    print("saving...")
     tools.write_csv_user(f"{folder}/user.csv",arr_user)
     tools.write_csv_bahan_bangunan(f"{folder}/bahan_bangunan.csv",arr_bahan_bangunan)
     tools.write_csv_candi(f"{folder}/candi.csv",arr_candi)
-    print(f"Berhasil menyimpan data di folder {folder}")
-    #f = open(f"{folder}/aa.txt",'w')
-    #f.write("asdasda")
-    #f.close()
+    print(f"Berhasil menyimpan data di folder {path}")

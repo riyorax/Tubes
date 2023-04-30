c018ecece9c5596e058d8c2e7855156e590e3336
riyorax <maximiliansulistiyo@gmail.com>
2023-04-30 22:23:48 +0700

    updates

--- F06.py
+++ F06.py
@@ -1,23 +1,28 @@
 import random
 import tools
 
-def jin_pembangun(file1,file2,array1,array2,id,pembuat):
+def jin_pembangun(arr_bahan_bangunan,arr_candi,pembuat):
     pasir = random.randint(1,5)
     batu = random.randint(1,5)
     air = random.randint(1,5)
-    array = tools.csv_array_bahan_bangunan("bahan_bangunan.csv",array1)
-    if int(array1[0][2])- pasir < 0 or int(array1[1][2])- batu < 0 or int(array1[2][2])- air < 0:
+    array = tools.csv_array_bahan_bangunan("bahan_bangunan.csv",arr_bahan_bangunan)
+    if int(arr_bahan_bangunan[1][2])- pasir < 0 or int(arr_bahan_bangunan[2][2])- batu < 0 or int(arr_bahan_bangunan[3][2])- air < 0:
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
     else:
-        a = int(array1[0][2])
+        a = int(arr_bahan_bangunan[1][2])
         a -= pasir
-        b = int(array1[1][2])
+        b = int(arr_bahan_bangunan[2][2])
         b -= batu
-        c = int(array1[2][2])
+        c = int(arr_bahan_bangunan[3][2])
         c -= air
-        array[0][2] = str(a)
-        array[1][2] = str(b)
-        array[2][2] = str(c)
-        tools.write_csv_candi(file2,array2,id,pembuat,pasir,batu,air)
-        tools.write_csv_bahan_bangunan(file1,array1)
+        arr_bahan_bangunan[0][2] = str(a)
+        arr_bahan_bangunan[1][2] = str(b)
+        arr_bahan_bangunan[2][2] = str(c)
+        id_candi = tools.id_candi(arr_candi)
+        flag = False
+        for i in range(101):
+            if arr_candi[i] == "-":
+                flag = True
+        if flag:
+            tools.write_array_candi(arr_candi,id_candi,pembuat,pasir,batu,air)

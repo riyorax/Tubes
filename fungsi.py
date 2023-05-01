e0afba75e52c875217542dade40d67f7677aba07
riyorax <maximiliansulistiyo@gmail.com>
2023-05-01 10:48:25 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -515,16 +515,16 @@ def hancurkancandi():
 #F12
 def ayamberkokok ():
     global candi
-    if tools.hitung_candi(candi) < 100 :
+    if hitung_candi(candi) < 100 :
         print("Kukuruyuk.. Kukuruyuk..")
-        print("Jumlah Candi: ", tools.hitung_candi(candi))
+        print("Jumlah Candi: ", hitung_candi(candi))
         print("Selamat, Roro Jonggrang memenangkan permainan!")
         print("*Bandung Bondowoso angry noise*")
         print("Roro Jonggrang dikutuk menjadi candi.")
         exit()
     else :
         print("Kukuruyuk.. Kukuruyuk..")
-        print("Jumlah Candi: ", tools.hitung_candi(candi))
+        print("Jumlah Candi: ", hitung_candi(candi))
         print("Yah, Bandung Bondowoso memenangkan permainan!")
         exit()
 #F13
